8f0f9e48924d06771d34ad505f6038fb794cea78
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2023-01-04 16:19:09 +0200

    add some pagination

--- src/index.js
+++ src/index.js
@@ -10,34 +10,59 @@ const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '32584793-3eb2bb36516e5beb26bc398d7';
 const SETTINGS = 'image_type=photo&orientation=horizontal&safesearch=true';
 
+let page = 1;
+let searchQuery = '';
+
+
 const form = document.querySelector('form');
 const divEl = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+
 form.addEventListener('submit', onSubmit);
+loadBtn.addEventListener('click', onClick);
 
 function onSubmit (evt) {
   evt.preventDefault();
-  //console.log('search', evt.currentTarget.elements[0].value);
-  fetchData(evt.currentTarget.elements[0].value);
-  evt.currentTarget.reset();
+  searchQuery = evt.currentTarget.elements.searchQuery.value
+  console.log('search', searchQuery);
+  if (searchQuery === '') {
+    //divEl.innerHTML = '';
+    return Notiflix.Notify.failure('Введіть будь ласка текст для пошуку!');  
+  }
+  fetchData(searchQuery);
+  //evt.currentTarget.reset();
 }
 
-async function fetchAPI (name) {
-    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}`);
-    //console.log('response', response);
+function onClick () {
+  page +=1;
+  console.log('page',page);
+  console.log('searchQuery', searchQuery);
+  
+  fetchData(searchQuery);
+
+}
+
+async function fetchAPI (name, page = 1 ) {
+    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}&page=${page}&per_page=40`);
+    console.log('response', response);
     return response.data;
 }
 
 async function fetchData (res) {
   try {
-    const data = await fetchAPI(res);
+    const data = await fetchAPI(res, page);
     //console.log('data', data);
     if (data.hits.length === 0) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         divEl.innerHTML = '';
         return;
       }
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    if (page === 1) {
+     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`); 
+    }  
     renderImages(data.hits);
+    loadBtn.classList.remove('visually-hidden');
+    console.log(page);
   } catch (error) {
     console.log ('Oй помилочка: ', error.message);
     Notiflix.Notify.failure('Упсс щось пішло не так... Спробуйте пізніше!');
@@ -55,10 +80,8 @@ function renderImages(data) {
         <p class="info-item"><span><b>Downloads</b></span><span>${downloads}</span></p>
       </div>
     </div>`).join('');
-    divEl.innerHTML = markup;
+    divEl.insertAdjacentHTML('beforeend', markup);
 }
-
-
 
 
 
