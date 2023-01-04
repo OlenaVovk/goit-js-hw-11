50336f70770e8045e1ffb567d04a622af4150172
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2023-01-04 11:35:25 +0200

    add async fetchData

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '32584793-3eb2bb36516e5beb26bc398d7';
@@ -10,36 +11,33 @@ const divEl = document.querySelector('.gallery');
 form.addEventListener('submit', onSubmit);
 
 function onSubmit (evt) {
-    evt.preventDefault();
-    fetchAPI(evt.currentTarget.elements[0].value)
-        .then(data => {
-            console.log('data', data);
-            if (data.hits.length === 0) {
-                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-                divEl.innerHTML = '';
-                return;
-            } 
-            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-            renderImages(data.hits);
-            console.log (data.hits.length);
-        
-            
-
-        })
-        .catch(error => console.log ('Упсс щось пішло не так: ', error.message))
-    evt.currentTarget.reset();
+  evt.preventDefault();
+  //console.log('search', evt.currentTarget.elements[0].value);
+  fetchData(evt.currentTarget.elements[0].value);
+  evt.currentTarget.reset();
 }
 
-
-
 async function fetchAPI (name) {
-    const response = await fetch(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}`);
-    console.log('response', response);
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-    const newImg = await response.json();
-    return newImg;
+    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}`);
+    //console.log('response', response);
+    return response.data;
+}
+
+async function fetchData (res) {
+  try {
+    const data = await fetchAPI(res);
+    //console.log('data', data);
+    if (data.hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        divEl.innerHTML = '';
+        return;
+      }
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    renderImages(data.hits);
+  } catch (error) {
+    console.log ('Oй помилочка: ', error.message);
+    Notiflix.Notify.failure('Упсс щось пішло не так... Спробуйте пізніше!');
+  }
 }
 
 function renderImages(data) {
@@ -63,6 +61,34 @@ function renderImages(data) {
     divEl.innerHTML = markup;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fetchAPI(evt.currentTarget.elements[0].value)
+//    .then(data => {
+//      if (data.hits.length === 0) {
+//        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//        divEl.innerHTML = '';
+//        return;
+//      }
+//      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//      renderImages(data.hits);
+//      })
+//    .catch(error => {
+//      console.log ('Oй помилочка: ', error.message);
+//      Notiflix.Notify.failure('Упсс щось пішло не так... Спробуйте пізніше!')
+//    })
 
 
 
