9c54597aa7bf4fa8652cb190a9291a287c490745
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2023-01-05 15:40:40 +0200

    add message of pagination end

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY_API = '32584793-3eb2bb36516e5beb26bc398d7';
 const SETTINGS = 'image_type=photo&orientation=horizontal&safesearch=true';
+const PAGE_SET = 40;
 
 let page = 1;
 let searchQuery = '';
-
-
+let max;
 
 const form = document.querySelector('form');
 const divEl = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
+const messageEl = document.querySelector('.message');
 
 form.addEventListener('submit', onSubmit);
 loadBtn.addEventListener('click', onClick);
@@ -26,7 +27,6 @@ function onSubmit (evt) {
   evt.preventDefault();
   page = 1;
   searchQuery = evt.currentTarget.elements.searchQuery.value
-  console.log('search', searchQuery);
   if (searchQuery === '') {
     divEl.innerHTML = '';
     loadBtn.classList.add('visually-hidden');
@@ -37,49 +37,55 @@ function onSubmit (evt) {
 }
 
 function onClick () {
-  page +=1;
-  console.log('page',page);
-  console.log('searchQuery', searchQuery);
-  
+  page +=1; 
+  if (page === max) {
+    loadBtn.classList.add('visually-hidden');
+    messageEl.classList.remove('visually-hidden');
+  }
   fetchData(searchQuery);
-
 }
 
 async function fetchAPI (name, page = 1 ) {
-    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}&page=${page}&per_page=40`);
-    console.log('response', response);
+    const response = await axios.get(`${BASE_URL}?key=${KEY_API}&q=${name}&${SETTINGS}&page=${page}&per_page=${PAGE_SET}`);
     return response.data;
 }
 
 async function fetchData (res) {
   try {
     const data = await fetchAPI(res, page);
-    //console.log('data', data);
+    max = Math.ceil(data.totalHits/PAGE_SET);
+    
     if (data.hits.length === 0) {
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       divEl.innerHTML = '';
       loadBtn.classList.add('visually-hidden');
+      messageEl.classList.add('visually-hidden');
       return;
     }
-    console.log('page1',page);
+    
     if (page === 1) {
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`); 
       insertImages(data.hits);
       loadBtn.classList.remove('visually-hidden');
+      messageEl.classList.add('visually-hidden');
     } else {
-      console.log('page2',page);
       addImages(data.hits);
     }
-    
-    
-  } catch (error) {
+
+    if (page === max) {
+    loadBtn.classList.add('visually-hidden');
+    messageEl.classList.remove('visually-hidden');
+    }
+  } 
+  
+  catch (error) {
     console.log ('Oй помилочка: ', error.message);
     Notiflix.Notify.failure('Упсс щось пішло не так... Спробуйте пізніше!');
   }
 }
 
 function makeMarkup(data) {
-    return data.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
+  return data.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
     `<div class="photo-card">
       <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
       <div class="info">
@@ -88,8 +94,7 @@ function makeMarkup(data) {
         <p class="info-item"><span><b>Comments</b></span><span>${comments}</span></p>
         <p class="info-item"><span><b>Downloads</b></span><span>${downloads}</span></p>
       </div>
-    </div>`).join('');
-    
+    </div>`).join('');   
 }
 
 function addImages(data) {
